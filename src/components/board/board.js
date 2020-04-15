72588207bc75ec302a7730af201e8a945f70e1cc
bharring52 <bharring52@hotmail.com>
2020-04-15 10:22:32 -0500

    Render board

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import { Space } from "./space/space.js";
 
-class Board extends React.Component {
-    renderSquare(i) {
-        return <Square />;
+export class Board extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.rows = 5;
+        this.columns = 10;
     }
-}
 
-class Square extends React.Component {
-    render () {
-        return (
-            <div />
+    populate() {
+        this.spaces = Array(this.rows).fill(
+            Array(this.columns).fill({})
         );
     }
+
+    render() {
+        this.populate();
+
+        return (
+            <div className="board">
+                {this.spaces.map((row, i) => this.renderRow(row, i))}
+            </div>
+        );
+    }
+
+    renderRow(row, key) {
+        return (
+            <div className="row" key={key}>
+                {row.map((space, i) =>
+                    this.renderSpace(space, i)
+                )}
+            </div>
+        );
+    }
+
+    renderSpace(space, key){
+        return <Space space={space} key={key}>fallback</Space>
+    };
 }
