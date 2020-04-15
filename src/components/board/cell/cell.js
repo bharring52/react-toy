ff8753ea37d05a0251d5ef8c31332969ca60f5b1
bharring52 <bharring52@hotmail.com>
2020-04-15 15:07:07 -0500

    Cleaner look, with colors instead of text

--- src/components/board/cell/cell.js
+++ src/components/board/cell/cell.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 export class Cell extends React.Component {
     render () {
+        const className = this.props.isAlive
+            ? 'cell alive'
+            : 'cell dead';
+
         return (
-            <div className="cell">
-                {this.props.isAlive ? "Alive" : ""}
+            <div className={className}>
             </div>
         );
     }
